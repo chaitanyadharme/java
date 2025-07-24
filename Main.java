bd049a3f35b8a02c7989b10058968c29a5b89ae4
Chaitanya Dharme <Chaitanydharme56@gmail.com>
2025-07-24 14:11:34 +0530

    Calculator

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args)
     {
+        int n1=20;
+        int n2=32;
+        Calculator cal= new Calculator();
+
+        int result = cal.add(n1,n2);
+        System.out.println(result);
         System.out.println("Hello World");    
+    }
+}
+
+class Calculator
+{
+    public int add(int m,int n)
+    {
+        int r;
+        r=m+n;
+        //System.out.println(r);
+        return r;
     }
 }
