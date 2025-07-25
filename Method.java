ff9dbd04a53707623f13008cb1dc4bed76357fc7
Chaitanya Dharme <Chaitanydharme56@gmail.com>
2025-07-25 14:27:25 +0530

    method

---
+++ Method.java
@@ -0,0 +1,23 @@
+public class MethodOverloading
+{
+    public static void main(String[] args) {
+        int a=20;
+        int b=25;
+        int c=94;
+
+        Overloading ov=new Overloading();
+        int result=ov.add(a,b,c);
+        System.out.println(result);
+    }
+}
+
+public class Overloading
+{
+    public int add(int x,int y,int z)
+    {
+        int ad=x+y+x;
+
+        return ad;
+    }
+
+}
